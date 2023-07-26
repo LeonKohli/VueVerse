10413b138d23ee3075abc02dc2db41cebdc046eb
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2023-07-26 15:06:18 +0200

    resize extra button component

--- src/components/MarkdownPreview.vue
+++ src/components/MarkdownPreview.vue
@@ -1,25 +1,40 @@
 <!-- src/components/MarkdownPreview.vue -->
 
 <template>
-  <div class="p-6 overflow-y-auto prose text-white bg-gray-900 rounded-lg shadow-lg preview dark:prose-invert" v-html="markdownContent"></div>
+  <div class="flex flex-col h-screen">
+    <ButtonBar />
+    <div
+      class="flex-grow w-full p-4 mb-4 overflow-y-auto prose text-white border border-gray-200 rounded-lg rounded-b-lg dark:prose-invert bg-gray-50 dark:border-gray-600 dark:text-white dark:bg-gray-800 markdown-preview"
+      v-html="markdownContent">
+    </div>
+  </div>
 </template>
 
-
 <style scoped>
-.preview {
-  min-height: 400px;
+.markdown-preview.prose {
+  min-height: calc(100vh - 2rem);
+  resize: none;
+  overflow-y: auto;
+  max-width: none !important;
+  word-wrap: break-word !important;
 }
 </style>
+
 <script lang="ts">
-import { marked } from "marked";
+import { marked } from 'marked'
 import { computed } from 'vue'
 import { useMarkdownStore } from '../stores/markdownStore'
-
+import ButtonBar from './ButtonBar.vue' // Import ButtonBar component
 
 export default {
+  components: {
+    ButtonBar // Register ButtonBar component
+  },
   setup() {
     const store = useMarkdownStore()
-    const markdownContent = computed(() => marked(store.content, { mangle: false, headerIds: false }))
+    const markdownContent = computed(() =>
+      marked(store.content, { mangle: false, headerIds: false })
+    )
 
     return { markdownContent }
   }
--- src/views/MarkdownView.vue
+++ src/views/MarkdownView.vue
@@ -1,13 +1,24 @@
 <!-- src/views/MarkdownView.vue -->
 
 <template>
-  <div class="flex h-screen p-10 bg-gray-700">
-    <MarkdownEditor class="w-1/2 mr-6" />
-    <MarkdownPreview class="w-1/2 ml-6" />
+  <div class="flex h-screen bg-gray-700">
+    <MarkdownEditor class="flex-grow" ref="editor" :style="{ width: `${editorWidth}px` }" />
+    <div class="cursor-col-resize" @mousedown="resizeStart" />
+    <MarkdownPreview class="flex-grow" ref="preview" :style="{ width: `${previewWidth}px` }" />
   </div>
 </template>
 
+<style scoped>
+.cursor-col-resize {
+  cursor: col-resize;
+  background-color: #444;
+  width: 10px;
+}
+</style>
+
+
 <script lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
 import MarkdownEditor from '../components/MarkdownEditor.vue'
 import MarkdownPreview from '../components/MarkdownPreview.vue'
 
@@ -15,9 +26,54 @@ export default {
   components: {
     MarkdownEditor,
     MarkdownPreview
+  },
+  setup() {
+    const editorWidth = ref(window.innerWidth / 2)
+    const previewWidth = ref(window.innerWidth / 2)
+
+
+
+    let isResizing = false
+
+    const resizeStart = () => {
+      isResizing = true
+      window.addEventListener('mousemove', resize)
+      window.addEventListener('mouseup', resizeEnd)
+    }
+
+    const resize = (e: MouseEvent) => {
+
+      if (!isResizing) return
+
+      editorWidth.value = e.clientX
+      previewWidth.value = window.innerWidth - e.clientX
+    }
+
+    const resizeEnd = () => {
+      isResizing = false
+      window.removeEventListener('mousemove', resize)
+      window.removeEventListener('mouseup', resizeEnd)
+    }
+
+    onMounted(() => {
+      window.addEventListener('resize', () => {
+        editorWidth.value = window.innerWidth / 2
+        previewWidth.value = window.innerWidth / 2
+      })
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', () => {
+        editorWidth.value = window.innerWidth / 2
+        previewWidth.value = window.innerWidth / 2
+      })
+    })
+
+    return {
+      editorWidth,
+      previewWidth,
+      resizeStart
+    }
   }
 }
 </script>
-
-<style scoped>
-</style>
